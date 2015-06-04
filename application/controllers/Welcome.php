2f598d1eea90ab3ce4d7d79ff243336514c4137a
brayanNunez <brayannr22@gmail.com>
2015-06-04 13:20:20 -0600

    mec
    mec

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		//prueba, vo me ta vacilando?????
+		//prueba, vo me ta vacilando??? mec
 		$this->load->view('welcome_message');
 	}
 }
