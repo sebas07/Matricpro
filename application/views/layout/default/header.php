b5a0f4d1a6deb2efbef0a5e98fa9cb4e03532d9d
brayanNunez <brayannr22@gmail.com>
2015-06-07 18:21:07 -0600

    menu fuera del header

--- application/views/layout/default/header.php
+++ application/views/layout/default/header.php
@@ -2,38 +2,6 @@
 <html>
 <head>
     <title><?php if(isset($title)) echo $title?></title>
-    <link href="<?=base_url()?>public/css/style.css" rel="stylesheet" type="text/css" />
     <link href="<?=base_url()?>public/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 </head>
-<body>
-    <div id="header">
-        <div id="nombreAplicacion">
-            <h3><?=APP_NAME?></h3>
-        </div>
 
-       <nav id="menu">
-           <ul id="top_menu">
-
-               <a href="<?=base_url()?>welcome/cerrarSesion"><li>Salir</li></a>
-               <a href="<?=base_url()?>usuario"><li>Administar</li></a>
-               <a href="<?=base_url()?>especialidad"><li>Especialidades</li></a>
-               <a href="<?=base_url()?>curso"><li>Cursos</li></a>
-               <a href="<?=base_url()?>profesor"><li>Profesor</li></a>
-               <a href="<?=base_url()?>estudiante"><li>Estudiante</li></a>
-               <a href="<?=base_url()?>welcome"><li>Inicio</li></a>
-
-           </ul>
-
-       </nav>
-
-
-    </div>
-    <div id="usuario">
-        <div id="usuariotexto">
-            <p>
-                <?=$usuario?>
-            </p>
-        </div>
-
-    </div>
-    <div id="contenido">
