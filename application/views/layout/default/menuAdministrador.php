ccca2baa83eace5a541d5dfd2ca36a383435bd01
sebas07 <josesrb10@outlook.com>
2015-06-07 20:05:42 -0600

    Menu administrador

--- application/views/layout/default/menuAdministrador.php
+++ application/views/layout/default/menuAdministrador.php
@@ -16,6 +16,21 @@
         </div>
     </nav>
 
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div style="align-content: center">
+                <ul class="nav navbar-nav" style="margin: 0 auto">
+                    <li><a href=""><strong>Principal</strong></a></li>
+                    <li><a href=""><strong>Carreras</strong></a></li>
+                    <li><a href=""><strong>Cursos</strong></a></li>
+                    <li><a href=""><strong>Profesores</strong></a></li>
+                    <li><a href=""><strong>Estudiantes</strong></a></li>
+                    <li><a href=""><strong>Administradores</strong></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
 
 </div>
 
