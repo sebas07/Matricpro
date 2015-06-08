3cf515674df12377583c951b0bc1703280436fea
brayanNunez <brayannr22@gmail.com>
2015-06-07 20:54:39 -0600

    centrar tabla

--- application/views/layout/default/header.php
+++ application/views/layout/default/header.php
@@ -3,5 +3,6 @@
 <head>
     <title><?php if(isset($title)) echo $title?></title>
     <link href="<?=base_url()?>public/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?=base_url()?>public/css/mystyle.css" rel="stylesheet" type="text/css" />
 </head>
 
--- application/views/layout/default/menuAdministrador.php
+++ application/views/layout/default/menuAdministrador.php
@@ -5,11 +5,11 @@
         <div class="container-fluid">
             <div style="align-content: center">
                 <ul class="nav navbar-nav" style="margin: 0 auto">
-                    <li><a href=""><strong>Principal</strong></a></li>
-                    <li><a href=""><strong>Carreras</strong></a></li>
+                    <li><a href="<?=base_url()?>welcome"><strong>Principal</strong></a></li>
+                    <li><a href="<?=base_url()?>carrera"><strong>Carreras</strong></a></li>
                     <li><a href=""><strong>Cursos</strong></a></li>
                     <li><a href=""><strong>Profesores</strong></a></li>
-                    <li><a href=""><strong>Estudiantes</strong></a></li>
+                    <li><a href="<?=base_url()?>estudiante"><strong>Estudiantes</strong></a></li>
                     <li><a href=""><strong>Administradores</strong></a></li>
                 </ul>
             </div>
