9d790fa16b7c41849ebc6d6e2864b6ca1b458462
brayanNunez <brayannr22@gmail.com>
2015-06-07 19:51:53 -0600

    menu

--- application/views/layout/default/menuAdministrador.php
+++ application/views/layout/default/menuAdministrador.php
@@ -1,21 +1,23 @@
 <body>
 <div id="header">
-    <nav id="menu">
-        <ul id="top_menu">
 
-            <a href="<?=base_url()?>welcome/cerrarSesion"><li>Salir</li></a>
-            <a href="<?=base_url()?>usuario"><li>Administar</li></a>
-            <a href="<?=base_url()?>especialidad"><li>Especialidades</li></a>
-            <a href="<?=base_url()?>curso"><li>Cursos</li></a>
-            <a href="<?=base_url()?>profesor"><li>Profesor</li></a>
-            <a href="<?=base_url()?>estudiante"><li>Estudiante</li></a>
-            <a href="<?=base_url()?>welcome"><li>Inicio</li></a>
-
-        </ul>
-
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div style="align-content: center">
+                <ul class="nav navbar-nav" style="margin: 0 auto">
+                    <li><a href=""><strong>Principal</strong></a></li>
+                    <li><a href=""><strong>Carreras</strong></a></li>
+                    <li><a href=""><strong>Cursos</strong></a></li>
+                    <li><a href=""><strong>Profesores</strong></a></li>
+                    <li><a href=""><strong>Estudiantes</strong></a></li>
+                    <li><a href=""><strong>Administradores</strong></a></li>
+                </ul>
+            </div>
+        </div>
     </nav>
 
 
 </div>
 
 <div id="contenido">
+
