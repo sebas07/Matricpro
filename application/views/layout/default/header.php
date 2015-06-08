2bb3bae35e562a86da2b6856f4209dcda2edd7af
sebas07 <josesrb10@outlook.com>
2015-06-07 21:44:08 -0600

    Centrar menu de administrador

--- application/views/layout/default/header.php
+++ application/views/layout/default/header.php
@@ -3,5 +3,26 @@
 <head>
     <title><?php if(isset($title)) echo $title?></title>
     <link href="<?=base_url()?>public/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <style>
+        .nav{
+            list-style:none;
+            margin:0;
+            padding:0;
+            text-align:center;
+        }
+        .nav li{
+            display:inline;
+        }
+        .nav li a{
+            display:inline-block;
+            width: 120px;
+            padding:10px;
+        }
+        .nav li a:hover{
+            background-color: darkgray;
+            color: #FFFFFF;
+            border-radius: 5px;
+        }
+    </style>
 </head>
 
--- application/views/layout/default/menuAdministrador.php
+++ application/views/layout/default/menuAdministrador.php
@@ -1,18 +1,16 @@
 <body>
 <div id="header">
 
-    <nav class="navbar navbar-default">
+    <nav id="meuA" class="navbar navbar-default">
         <div class="container-fluid">
-            <div style="align-content: center">
-                <ul class="nav navbar-nav" style="margin: 0 auto">
-                    <li><a href=""><strong>Principal</strong></a></li>
-                    <li><a href=""><strong>Carreras</strong></a></li>
-                    <li><a href=""><strong>Cursos</strong></a></li>
-                    <li><a href=""><strong>Profesores</strong></a></li>
-                    <li><a href="<?= base_url(); ?>index.php/estudiante/"><strong>Estudiantes</strong></a></li>
-                    <li><a href=""><strong>Administradores</strong></a></li>
-                </ul>
-            </div>
+            <ul class="nav">
+                <li><a href=""><strong>Principal</strong></a></li>
+                <li><a href=""><strong>Carreras</strong></a></li>
+                <li><a href=""><strong>Cursos</strong></a></li>
+                <li><a href=""><strong>Profesores</strong></a></li>
+                <li><a href="<?= base_url(); ?>index.php/estudiante/"><strong>Estudiantes</strong></a></li>
+                <li><a href=""><strong>Administradores</strong></a></li>
+            </ul>
         </div>
     </nav>
 
