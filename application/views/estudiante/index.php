852373d6d83161ba62b93a3e06b9468a8f0d09e4
brayanNunez <brayannr22@gmail.com>
2015-06-10 15:00:31 -0600

    arreglar estylo

--- application/views/estudiante/index.php
+++ application/views/estudiante/index.php
@@ -12,22 +12,6 @@
     <?php foreach($js_files as $file): ?>
         <script src="<?php echo $file; ?>"></script>
     <?php endforeach; ?>
-    <style type='text/css'>
-        body
-        {
-            font-family: Arial;
-            font-size: 14px;
-        }
-        a {
-            color: blue;
-            text-decoration: none;
-            font-size: 14px;
-        }
-        a:hover
-        {
-            text-decoration: underline;
-        }
-    </style>
 </head>
 <body>
 <div style='height:20px;'></div>
--- application/views/layout/default/header.php
+++ application/views/layout/default/header.php
@@ -6,25 +6,25 @@
 
     <link href="<?=base_url()?>assets/css/mystyle.css" rel="stylesheet" type="text/css" />
     <style>
-        .nav{
-            list-style:none;
-            margin:0;
-            padding:0;
-            text-align:center;
-        }
-        .nav li{
-            display:inline;
-        }
-        .nav li a{
-            display:inline-block;
-            width: 120px;
-            padding:10px;
-        }
-        .nav li a:hover{
-            background-color: darkgray;
-            color: #FFFFFF;
-            border-radius: 5px;
-        }
+        /*.nav{*/
+            /*list-style:none;*/
+            /*margin:0;*/
+            /*padding:0;*/
+            /*text-align:center;*/
+        /*}*/
+        /*.nav li{*/
+            /*display:inline;*/
+        /*}*/
+        /*.nav li a{*/
+            /*display:inline-block;*/
+            /*width: 120px;*/
+            /*padding:10px;*/
+        /*}*/
+        /*.nav li a:hover{*/
+            /*background-color: darkgray;*/
+            /*color: #FFFFFF;*/
+            /*border-radius: 5px;*/
+        /*}*/
     </style>
 </head>
 
