1ebe2246e0649a6fbd96a761fe9f02455f0d34a3
jorarias <jorge.ariaschaves@ucrso.info>
2015-06-27 14:39:21 -0600

    uno q falto

--- application/views/layout/default/menuEstudiante.php
+++ application/views/layout/default/menuEstudiante.php
@@ -3,9 +3,9 @@
     <nav id="meuA" class="navbar navbar-default">
         <div class="container-fluid">
             <ul class="nav">
-                <li><a href="<?=base_url()?>inicioEstudiante"><strong>Principal</strong></a></li>
-                <li><a href="<?=base_url()?>inicioEstudiante"><strong>Matricular</strong></a></li>
-                <li><a href="<?=base_url()?>inicioEstudiante"><strong>Expediente Académico</strong></a></li>
+                <li><a href="<?=base_url()?>InicioEstudiante"><strong>Principal</strong></a></li>
+                <li><a href="<?=base_url()?>Matricula/matricular"><strong>Matricular</strong></a></li>
+                <li><a href="<?=base_url()?>InicioEstudiante"><strong>Expediente Académico</strong></a></li>
                 <li><a class="btn btn-danger" href="<?=base_url()?>InicioEstudiante/logout">Salir</a></li>
             </ul>
         </div>
