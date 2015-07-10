7ddcb9d16cfcb37979a03ae04f1c95524bee29d1
sebas07 <josesrb10@outlook.com>
2015-07-09 23:24:17 -0600

    Imagen y titulo de la pestanna

--- application/views/layout/default/header.php
+++ application/views/layout/default/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title><?php if(isset($title)) { echo $title; } else { echo 'Matricpro'; } ?></title>
+    <link href="<?=base_url()?>assets/images/icono.png" rel="icon" type="image/gif" />
     <link href="<?=base_url()?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="<?=base_url()?>assets/css/mystyle.css" rel="stylesheet" type="text/css" />
 </head>
